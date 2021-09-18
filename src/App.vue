db5c10e86d004e4a5af3478e462fb141aeeea102
GuillaumeChk <guillaume.chekhab39@gmail.com>
2021-09-18 21:40:46 +0200

    page Connexion commencée

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Logo Grouporama" src="../public/icones/icon.svg">
+  <h1>Grouporama</h1>
+  <Connexion />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+// <script>
+import Connexion from './components/Connexion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Connexion
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  img {
+    max-width: 30%;
+  }
 </style>
---
+++ src/components/Connexion.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <h3>Connexion</h3>
+        <button class="selected">Se connecter</button>
+        <button>S'enregistrer</button>
+        <form action="">
+        <div>
+            <label for="">Mail : </label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Mot de passe : </label>
+            <input type="text">
+        </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Connexion',
+    props: {
+        signOrLogIn: Boolean
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        border: 1px solid;
+        border-radius: 5px;
+    }
+    button {
+        width: auto;
+        padding: 20px;
+        font-size: 1.5rem;
+        margin-bottom: 20px;
+    }
+    .selected {
+        color: rgb(35, 35, 255);
+    }
+    div {
+        margin-bottom: 15px;
+    }
+</style>
